dbc85410a69c653b974bd74348844bd09d2f2b3a
Max Niederman <MNiederman2082@student.pps.net>
2022-03-10 16:46:54 -0800

    feat(camera): implement light switch

--- src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -4,6 +4,11 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
@@ -11,11 +16,20 @@ import org.photonvision.targeting.PhotonPipelineResult;
 public class CameraSubsystem extends SubsystemBase {
 
     private final PhotonCamera camera = new PhotonCamera("goalvision");
-    private double distanceToGoal = 0;
+    Socket lightSocket;
 
+    private double distanceToGoal = 0;
     private double angleToGoal = 0;
 
-    NetworkTable light = NetworkTableInstance.getDefault().getTable("vision");
+    public CameraSubsystem() {
+        super();
+
+        try {
+            lightSocket = new Socket("photonvision.local", 4000);
+        } catch (Exception e) {
+            System.err.println("ERROR: Could not connect to camera light server.");
+        }
+    }
 
     @Override
     public void periodic() {
@@ -45,10 +59,18 @@ public class CameraSubsystem extends SubsystemBase {
     }
 
     public void turnOnLight() {
-        light.getEntry("vision").setBoolean(true);
+        try {
+            lightSocket.getOutputStream().write('1');
+        } catch (Exception e) {
+            System.err.println("WARN: failed to turn on camera light");
+        }
     }
 
     public void turnOffLight() {
-        light.getEntry("vision").setBoolean(false);
+        try {
+            lightSocket.getOutputStream().write('0');
+        } catch (Exception e) {
+            System.err.println("WARN: failed to turn off camera light");
+        }
     }
 }
